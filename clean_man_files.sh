ef008eb80cbf32f0c4e7d5ea63554e107894a9ba
3L0C <141392286+3L0C@users.noreply.github.com>
2024-04-10 18:22:09 -0700

    Working on man pages

--- clean_man_files.sh
+++ clean_man_files.sh
@@ -9,10 +9,12 @@ new_date=$(date '+%B %d, %Y')
 fix_header="s/^\.TH \(\"[^\"]*\"\) \(\"[^\"]*\"\).*$/.TH \1 \2 \"$new_date\" \"\" \"$file_type\"/"
 remove_zero_width_space="s/\xe2\x80\x8b//g"
 fix_bold_caret="s/\*\^\*\^/\\\\f[B]^\\\\f[R]/"
+remove_surrounding_tilde="s/\~\([^\~ ]\+\)\~/_\1/g"
 
 # Update man heading with the new date and file type and remove zero-width-spaces
 sed -i \
     -e "$fix_header" \
     -e "$remove_zero_width_space" \
     -e "$fix_bold_caret" \
+    -e "$remove_surrounding_tilde" \
     "$file_name"
