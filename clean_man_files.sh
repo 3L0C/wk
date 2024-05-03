56555da872ab52e22fd72ba7333286f204b22060
3L0C <141392286+3L0C@users.noreply.github.com>
2024-05-03 00:37:48 -0700

    Fix some man formatting

--- clean_man_files.sh
+++ clean_man_files.sh
@@ -9,6 +9,7 @@ new_date=$(date '+%B %d, %Y')
 fix_header="s/^\.TH \(\"[^\"]*\"\) \(\"[^\"]*\"\).*$/.TH \1 \2 \"$new_date\" \"\" \"$file_type\"/"
 remove_zero_width_space="s/\xe2\x80\x8b//g"
 fix_bold_caret="s/\*\^\*\^/\\\\f[B]^\\\\f[R]/"
+fix_interp_caret="s/\*desc\\\\\[ha]\^\*\^/\\\\f[B]desc^^\\\\f[R]/;s/\*desc\^\*\^/\\\\f[B]desc^\\\\f[R]/"
 remove_surrounding_tilde="s/\~\([^\~ ]\+\)\~/_\1/g"
 
 # Update man heading with the new date and file type and remove zero-width-spaces
@@ -16,5 +17,6 @@ sed -i \
     -e "$fix_header" \
     -e "$remove_zero_width_space" \
     -e "$fix_bold_caret" \
+    -e "$fix_interp_caret" \
     -e "$remove_surrounding_tilde" \
     "$file_name"
