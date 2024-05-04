4d9fc33d81b82b076f3ba90c4256210437a4cae5
3L0C <141392286+3L0C@users.noreply.github.com>
2024-05-03 19:23:04 -0700

    Make config consistent

--- config/config.def.h
+++ config/config.def.h
@@ -12,30 +12,30 @@ static const char* delimiter = " -> ";
 static uint32_t delay = 1000;
 /* Max number of columns to use. */
 static const uint32_t maxCols = 5;
-/* Window width. Set to '-1' for 1/2 the width of your screen. */
-static const int32_t windowWidth = -1;
-/* Window gap between top/bottom of screen. Set to '-1' for a gap of 1/10th of the screen height. */
-static const int32_t windowGap = -1;
+/* Menu width. Set to '-1' for 1/2 the width of your screen. */
+static const int32_t menuWidth = -1;
+/* Menu gap between top/bottom of screen. Set to '-1' for a gap of 1/10th of the screen height. */
+static const int32_t menuGap = -1;
 /* X-Padding around key/description text in cells. */
 static const uint32_t widthPadding = 6;
 /* Y-Padding around key/description text in cells. */
 static const uint32_t heightPadding = 2;
-/* Position to place the window. '0' = bottom; '1' = top. */
-static const uint32_t windowPosition = 0;
-/* Window border width */
+/* Position to place the menu. '0' = bottom; '1' = top. */
+static const uint32_t menuPosition = 0;
+/* Menu border width */
 static const uint32_t borderWidth = 4;
-/* Window border radius. 0 means no curve */
+/* Menu border radius. 0 means no curve */
 static const double borderRadius = 0;
-/* Window foreground color */
+/* Menu foreground color */
 static const char* foreground[FOREGROUND_COLOR_LAST] = {
     "#DCD7BA", /* Key color */
     "#525259", /* Delimiter color */
     "#AF9FC9", /* Prefix color */
     "#DCD7BA", /* Chord color */
 };
-/* Window background color */
+/* Menu background color */
 static const char* background = "#181616";
-/* Window border color */
+/* Menu border color */
 static const char* border = "#7FB4CA";
 /* Default shell to run chord commands with. */
 static const char* shell = "/bin/sh";
